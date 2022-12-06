6454fe495ab3085f0e2b5457f7146757aed09858
Asif Rahman <asif@crandonventures.com>
2022-12-06 02:51:31 -0500

    commit tree ac4d533ed5b1de688936b3ba0ed9bf793e526f8d

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "Jim",
+    streetAddress: "11 Broadway"
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    employee[key] = value;
+    return employee
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+    const newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete employee[key]
+    return employee
+}
